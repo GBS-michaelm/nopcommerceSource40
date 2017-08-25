12b0d6248018a868f312169520ce2e46420227a8
Clint Treadway <clint@gogbs.com>
2017-08-25 07:22:42 -0700

    Unsubscribe Project

---
+++ Plugins/Nop.Plugin.Widgets.Marketing/Models/EmailPrefModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using WebServices.Models;
+
+namespace Nop.Plugin.Widgets.Marketing.EmailPref.Models
+{
+
+    public class EmailListModel
+    {
+        public int Id { get; set; }
+        public string ListHeader { get; set; }
+        public string ListTag { get; set; }
+        public string ListDetails { get; set; }
+        public string ListImage { get; set; }
+        public string SubListName { get; set; }
+        public string UnSubListName { get; set; }
+        public bool SendWeekly { get; set; }
+        public bool SendMonthly { get; set; }
+        public bool Unsubscribe { get; set; }
+        public string ListWebsite { get; set; }
+        public bool Master { get; set; }
+        public bool Active { get; set; }
+        public bool listSubscribeStatus
+        {
+            get { return _listSubscribeStatus; }
+            set { _listSubscribeStatus = value; }
+        }
+
+        private bool _listSubscribeStatus = false;
+
+    }
+
+    public class EmailPreferencesModel
+    {
+        public IList<EmailListModel> ShowLists { get; set; }
+        public IList<EmailListModel> ShowPartnerLists { get; set; }
+        public MarketingContactModel ContactPref { get; set; }
+        public string subscribeStatusMaster { get; set; }
+        public string subscribeStatusPartner { get; set; }
+
+        public EmailPreferencesModel()
+        {
+            ShowLists = new List<EmailListModel>();
+            ShowPartnerLists = new List<EmailListModel>();
+            ContactPref = new MarketingContactModel();
+        }
+
+    }
+
+    public class EmailPreferencesErrorModel
+    {
+        public string ErrorMessage { get; set; }
+    }
+}
