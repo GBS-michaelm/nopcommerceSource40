3e77a0517a8d89c227c5e5ea2915058819a7cfe4
Vic Rivera <Vic@gogbs.com>
2017-10-27 11:48:44 -0700

    company office team gateway update

--- Plugins/Nop.Plugin.BusinessLogic.GBS/Domain/Company.cs
+++ Plugins/Nop.Plugin.BusinessLogic.GBS/Domain/Company.cs
@@ -5,32 +5,70 @@ using System.Text;
 using System.Threading.Tasks;
 using Nop.Plugin.BusinessDataAccess.GBS;
 using System.Data;
+using Nop.Web.Models.Catalog;
+using Nop.Core.Domain.Catalog;
+using Nop.Web.Factories;
+using Nop.Core.Infrastructure;
+using Nop.Services.Catalog;
+using Nop.Services.Vendors;
+using Nop.Core;
+using Nop.Services.Directory;
+using Nop.Services.Media;
+using Nop.Services.Localization;
+using Nop.Services.Security;
+using Nop.Services.Stores;
+using Nop.Services.Topics;
+using Nop.Services.Events;
+using Nop.Services.Common;
+using Nop.Core.Caching;
+using System.Web;
 
 namespace Nop.Plugin.BusinessLogic.GBS.Domain
 {
-    public class Company
+    public class Company : CategoryModel
     {
 
         DBManager manager = new DBManager();
        
-        private int _categoryId = 0;
-        private int _parentCategoryId = 0;
-        private string _name = "";
+        private int _id = 0;
+        private int _parentCategoryId = 0;    
         private string _h1 = "";
-        private string _h2 = "";
-        private string _backgroundColor = "";
-        private bool _isCompany = false;
+        private string _h2 = "";         
         private string _logoPicturePath = "";
-        private string _landingHeader = "";
-        private string _aboutYourMarketCenter = "";
-        private int _statusId = 0;
-        private string _forgroundColor = "#000000";        
-        private string _description = "";
+        private string _aboutYourMarketCenter = "";       
+        private string _forgroundColor = "#000000";
 
-        public Company(string companyId)
+        //private string _backgroundColor = ""; 
+        //private string _landingHeader = "";
+        //private int _statusId = 0;
+
+        public Company(int companyId)
         {
-            //datalook up via company id
-            Dictionary<string, string> companyDic = new Dictionary<string, string>();
+            //nop category data
+            ICategoryService categoryService = EngineContext.Current.Resolve<ICategoryService>();
+            this.id = companyId;
+            Category category = categoryService.GetCategoryById(companyId);
+            ICatalogModelFactory catalogModelFactory = EngineContext.Current.Resolve<ICatalogModelFactory>();           
+            CatalogPagingFilteringModel catalogPagingFilteringModel = new CatalogPagingFilteringModel();            
+            catalogPagingFilteringModel.PageSize = 1;
+            this.PagingFilteringContext = catalogPagingFilteringModel;
+            CategoryModel categoryModel = catalogModelFactory.PrepareCategoryModel(category, catalogPagingFilteringModel);          
+            this.Name = categoryModel.Name;
+            this.Description = categoryModel.Description;
+            this.MetaKeywords = categoryModel.MetaKeywords;
+            this.MetaDescription = categoryModel.MetaDescription;
+            this.MetaTitle = categoryModel.MetaTitle;
+            this.SeName = categoryModel.SeName;
+            this.PictureModel = categoryModel.PictureModel;
+            this.PagingFilteringContext = categoryModel.PagingFilteringContext;
+            this.DisplayCategoryBreadcrumb = categoryModel.DisplayCategoryBreadcrumb;
+            this.CategoryBreadcrumb = categoryModel.CategoryBreadcrumb;
+            this.SubCategories = categoryModel.SubCategories;
+            this.FeaturedProducts = categoryModel.FeaturedProducts;
+            this.Products = categoryModel.Products;
+            
+            //datalook up via category(company) id from gbscategory table
+            Dictionary<string, Object> companyDic = new Dictionary<string, Object>();
             companyDic.Add("@CategoryId", companyId);
 
             string companyDataQuery = "EXEC usp_SelectGBSCompanyData @CategoryId";
@@ -38,38 +76,34 @@ namespace Nop.Plugin.BusinessLogic.GBS.Domain
 
             if(companyDataView.Count > 0)
             {
-                this.categoryId = Int32.Parse(companyDataView[0]["CategoryID"].ToString());
-                int parentCatId;
-                this.parentCategoryId = Int32.TryParse(companyDataView[0]["ParentCategoryID"].ToString(), out parentCatId) ? parentCatId : _parentCategoryId;
-                this.name = !string.IsNullOrEmpty(companyDataView[0]["Name"].ToString()) ? companyDataView[0]["Name"].ToString() : _name;
-                this.h1 = !string.IsNullOrEmpty(companyDataView[0]["H1"].ToString()) ? companyDataView[0]["H1"].ToString() : this.name;
-                this.h2 = !string.IsNullOrEmpty(companyDataView[0]["H2"].ToString()) ? companyDataView[0]["H2"].ToString() : _h2;
-                this.backgroundColor = !string.IsNullOrEmpty(companyDataView[0]["BackgroundColor"].ToString()) ? companyDataView[0]["BackgroundColor"].ToString() : _backgroundColor;
-                this.isCompany = (bool)companyDataView[0]["IsCompany"];
+                
+                this.parentCategoryId = category.ParentCategoryId;               
+                this.h1 = !string.IsNullOrEmpty(companyDataView[0]["H1"].ToString()) ? companyDataView[0]["H1"].ToString() : this.Name;
+                this.h2 = !string.IsNullOrEmpty(companyDataView[0]["H2"].ToString()) ? companyDataView[0]["H2"].ToString() : _h2;                
                 this.logoPicturePath = !string.IsNullOrEmpty(companyDataView[0]["LogoPicturePath"].ToString()) ? companyDataView[0]["LogoPicturePath"].ToString() : _logoPicturePath;
-                this.landingHeader = !string.IsNullOrEmpty(companyDataView[0]["LandingHeader"].ToString()) ? companyDataView[0]["LandingHeader"].ToString() : _landingHeader;
-                this.aboutYourMarketCenter = !string.IsNullOrEmpty(companyDataView[0]["aboutYourMarketCenter"].ToString()) ? companyDataView[0]["aboutYourMarketCenter"].ToString() : _aboutYourMarketCenter;
-                int statId;
-                this.statusId = Int32.TryParse(companyDataView[0]["StatusID"].ToString(), out statId) ? statId : _statusId;
-                this.foregroundColor = !string.IsNullOrEmpty(companyDataView[0]["ForegroundColor"].ToString()) ? companyDataView[0]["ForegroundColor"].ToString() : _forgroundColor;           
-                this.description = !string.IsNullOrEmpty(companyDataView[0]["Description"].ToString()) ? companyDataView[0]["Description"].ToString() : _description;             
+                this.aboutYourMarketCenter = !string.IsNullOrEmpty(companyDataView[0]["aboutYourMarketCenter"].ToString()) ? companyDataView[0]["aboutYourMarketCenter"].ToString() : _aboutYourMarketCenter;               
+                this.foregroundColor = !string.IsNullOrEmpty(companyDataView[0]["ForegroundColor"].ToString()) ? companyDataView[0]["ForegroundColor"].ToString() : _forgroundColor;
+
+                //this.backgroundColor = !string.IsNullOrEmpty(companyDataView[0]["BackgroundColor"].ToString()) ? companyDataView[0]["BackgroundColor"].ToString() : _backgroundColor;
+                //this.landingHeader = !string.IsNullOrEmpty(companyDataView[0]["LandingHeader"].ToString()) ? companyDataView[0]["LandingHeader"].ToString() : _landingHeader;
+                //int statId;
+                //this.statusId = Int32.TryParse(companyDataView[0]["StatusID"].ToString(), out statId) ? statId : _statusId;
+
             }
 
         }
         
-        public int categoryId { get { return _categoryId; } set { _categoryId = value; } }
-        public int parentCategoryId { get { return _parentCategoryId; } set { _parentCategoryId = value; } }
-        public string name { get { return _name; } set { _name = value; } }
+        public int id { get { return _id; } set { _id = value; } }
+        public int parentCategoryId { get { return _parentCategoryId; } set { _parentCategoryId = value; } }     
         public string h1 { get { return _h1; } set { _h1 = value; } }
-        public string h2 { get { return _h2; } set { _h2 = value; } }
-        public string backgroundColor { get { return _backgroundColor; } set { _backgroundColor = value; } }
-        public bool isCompany { get { return _isCompany; } set { _isCompany = value; } }
+        public string h2 { get { return _h2; } set { _h2 = value; } }   
         public string logoPicturePath { get { return _logoPicturePath; } set { _logoPicturePath = value; } }
-        public string landingHeader { get { return _landingHeader; } set { _landingHeader = value; } }
         public string aboutYourMarketCenter { get { return _aboutYourMarketCenter; } set { _aboutYourMarketCenter = value; } }
-        public int statusId { get { return _statusId; } set { _statusId = value; } }
         public string foregroundColor { get { return _forgroundColor; } set { _forgroundColor = value; } }
-        public string description { get { return _description; } set { _description = value; } }       
 
+        //public string backgroundColor { get { return _backgroundColor; } set { _backgroundColor = value; } }
+        //public string landingHeader { get { return _landingHeader; } set { _landingHeader = value; } }
+        //public int statusId { get { return _statusId; } set { _statusId = value; } }
+        
     }
 }
