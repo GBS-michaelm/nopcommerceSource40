04b67c4a75d34d6dd4eb03367d068ef332508c59
Clint Treadway <clint@gogbs.com>
2017-11-21 14:27:54 -0800

    Updates

--- Plugins/Nop.Plugin.Payments.PurchaseOrder/Models/PaymentInfoModel.cs
+++ Plugins/Nop.Plugin.Payments.PurchaseOrder/Models/PaymentInfoModel.cs
@@ -1,13 +1,16 @@
 ﻿using System.Web.Mvc;
 using Nop.Web.Framework;
 using Nop.Web.Framework.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Nop.Plugin.Payments.PurchaseOrder.Models
 {
     public class PaymentInfoModel : BaseNopModel
     {
         [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.PurchaseOrderNumber")]
-        [AllowHtml]
+        //[AllowHtml]
         public string PurchaseOrderNumber { get; set; }
+        public string PurchaseOrderName { get; set; }
+        public string PurchaseOrderPhoneNumber { get; set; }
     }
 }
