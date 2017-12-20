8b05c002ac62b1774eb4aedec0873b3895392054
Joe Barrus <joe@gogbs.com>
2017-12-19 17:36:52 -0800

    stuff

---
+++ plugins/Nop.Plugin.Products.SpecificationAttributes/Models/OrderHistoryPagingFilteringModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Nop.Web.Framework.UI.Paging;
+
+namespace Nop.Plugin.Products.SpecificationAttributes.Models
+{
+    public partial class OrderHistoryPagingFilteringModel : BasePageableModel
+    {
+        #region Ctor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OrderHistoryPagingFilteringModel()
+        {
+            this.PageSizeOptions = new List<SelectListItem>();
+        }
+
+        #endregion
+
+        #region Properties
+
+
+        /// <summary>
+        /// Available page size options
+        /// </summary>
+        public IList<SelectListItem> PageSizeOptions { get; set; }
+
+    }
+}
