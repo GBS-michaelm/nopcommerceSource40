0754cee5f301695226104a775f3c146a413d5fdf
Joe Barrus <joe@gogbs.com>
2018-01-05 12:42:31 -0800

    sportsteam caching

---
+++ Plugins/Nop.Plugin.Catalog.GBS/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using Nop.Web.Framework.Controllers;
+using Nop.Services.Catalog;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Core;
+using Nop.Services.Stores;
+using Nop.Web.Factories;
+using System.Web.Mvc;
+using Nop.Core.Domain.Catalog;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Catalog.GBS.Controllers
+{
+    public class GBSCatalogController : BaseController
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly ICatalogModelFactory _catalogModelFactory;
+
+        private readonly IWorkContext _workContext;
+        private readonly IStoreService _storeService;
+        private readonly ISettingService _settingService;
+        private readonly ILocalizationService _localizationService;
+
+        public GBSCatalogController(
+            ICategoryService categoryService,
+            ICatalogModelFactory catalogModelFactory,
+            IWorkContext workContext,
+            IStoreService storeService,
+            ISettingService settingService,
+            ILocalizationService localizationService)
+        {
+            this._categoryService = categoryService;
+            this._catalogModelFactory = catalogModelFactory;
+
+            this._workContext = workContext;
+            this._storeService = storeService;
+            this._settingService = settingService;
+            this._localizationService = localizationService;
+        }
+
+        [HttpPost]
+        public ActionResult SportsTeamsList(IList<Category> model, int sportsTeamId)
+        {
+            ViewBag.Id = sportsTeamId;
+            return View(model);
+        }
+    }
+}
