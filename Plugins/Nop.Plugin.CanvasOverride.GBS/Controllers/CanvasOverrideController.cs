cbec1637a9ec9e6802666c63e89d60c673832f09
alexk <adotkang@gmail.com>
2018-05-23 10:37:27 -0700

    Dev

---
+++ Plugins/Nop.Plugin.CanvasOverride.GBS/Controllers/CanvasOverrideController.cs
@@ -0,0 +1,74 @@
+﻿using Nop.Core;
+using Nop.Plugin.Widgets.CustomersCanvas;
+using Nop.Plugin.Widgets.CustomersCanvas.Controllers;
+using Nop.Plugin.Widgets.CustomersCanvas.Services;
+using Nop.Services.Catalog;
+using Nop.Services.Configuration;
+using Nop.Services.Media;
+using Nop.Services.Orders;
+using Nop.Web.Controllers;
+
+namespace Nop.Plugin.CanvasOverride.GBS.Controllers
+{
+    public partial class CanvasOverrideController : CcWidgetController
+    {
+        #region Fields
+
+        private readonly IProductService _productService;
+        private readonly IOrderService _orderService;
+        private readonly ICcService _ccService;
+        private readonly IShoppingCartService _cartService;
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+        private readonly IProductAttributeService _productAttributeService;
+        private readonly ISettingService _settingService;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IPictureService _pictureService;
+        private readonly IProductAttributeParser _productAttributeParser;
+        private readonly CcSettings _customersCanvasSettings;
+        private readonly IWebHelper _webHelper;
+        private readonly ISpecificationAttributeService _specificationAttributeService;
+        private readonly ProductController _productController;
+        private readonly ShoppingCartController _shoppingCartController;
+        private readonly IDownloadService _downloadService;
+
+        #endregion Fields
+
+        #region ctor
+
+        public CanvasOverrideController(IProductService productService,
+            IOrderService orderService,
+            ICcService ccService,
+            IShoppingCartService cartService,
+            IWorkContext workContext,
+            IStoreContext storeContext,
+            IProductAttributeService productAttributeService,
+            ISettingService settingService,
+            IPictureService pictureService,
+            IProductAttributeParser productAttributeParser,
+            IShoppingCartService shoppingCartService,
+            IWebHelper webHelper,
+            ISpecificationAttributeService specificationAttributeService,
+            IDownloadService downloadService) : base(
+                productService,
+                orderService,
+                ccService,
+                cartService,
+                workContext,
+                storeContext,
+                productAttributeService,
+                settingService,
+                pictureService,
+                productAttributeParser,
+                shoppingCartService,
+                webHelper,
+                specificationAttributeService,
+                downloadService
+                )
+        {
+
+        }
+
+        #endregion ctor
+    }
+}
---
+++ Plugins/Nop.Plugin.CanvasOverride.GBS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nop.Plugin.CanvasOverride.GBS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nop.Plugin.CanvasOverride.GBS")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("54d3de92-52c8-43d7-9f3f-6279d1aac8e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
